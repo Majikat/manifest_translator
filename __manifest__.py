bee1571d50d03d3b6c81ab38e775fc72fb22cba8
dan-raoelinarivo <dan.raoelinarivo@live.fr>
2016-10-05 11:34:10 +0300

    Initial commit and V1 realease.

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Manifest Translator",
+
+    'summary': """
+Simplest manifest translator for my custom modules.
+    """,
+
+    'description': """
+This module translates my custom modules manifests.
+    """,
+
+    'author': "Majikat",
+    'website': "http://majikat.com",
+
+    'category': 'Uncategorized',
+    'version': '1.0.0',
+
+    'depends': ['base'],
+
+    'data': [],
+    'installable': True,
+    'auto_install': True,
+    'bootstrap': True
+}
